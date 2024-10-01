def38420f65aa91b9e401bd7fd4fd7149863d516
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2024-10-01 15:36:13 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://bookstorebackend-0lm0.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(userInfo);
         console.log(res.data);
